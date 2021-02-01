1d578cb33d87c417a5ef9b8cb0eb3271cb363b65
Yevgeni Anischenko <yevkemail@gmail.com>
2021-02-01 23:11:12 +0200

    Add project files.

---
+++ EmployeeSystem/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace EmployeeSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtboxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtboxSurname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtboxPhone = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtboxAddress = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbGender = new System.Windows.Forms.ComboBox();
+            this.cbRole = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // txtboxName
+            // 
+            this.txtboxName.Location = new System.Drawing.Point(160, 48);
+            this.txtboxName.Name = "txtboxName";
+            this.txtboxName.Size = new System.Drawing.Size(121, 22);
+            this.txtboxName.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name:";
+            // 
+            // txtboxSurname
+            // 
+            this.txtboxSurname.Location = new System.Drawing.Point(160, 106);
+            this.txtboxSurname.Name = "txtboxSurname";
+            this.txtboxSurname.Size = new System.Drawing.Size(121, 22);
+            this.txtboxSurname.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Surname:";
+            // 
+            // txtboxPhone
+            // 
+            this.txtboxPhone.Location = new System.Drawing.Point(160, 165);
+            this.txtboxPhone.Name = "txtboxPhone";
+            this.txtboxPhone.Size = new System.Drawing.Size(121, 22);
+            this.txtboxPhone.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Phone";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Address:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(69, 331);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Gender:";
+            // 
+            // txtboxAddress
+            // 
+            this.txtboxAddress.Location = new System.Drawing.Point(160, 221);
+            this.txtboxAddress.Multiline = true;
+            this.txtboxAddress.Name = "txtboxAddress";
+            this.txtboxAddress.Size = new System.Drawing.Size(121, 80);
+            this.txtboxAddress.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(69, 385);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Role:";
+            // 
+            // cbGender
+            // 
+            this.cbGender.FormattingEnabled = true;
+            this.cbGender.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Other"});
+            this.cbGender.Location = new System.Drawing.Point(160, 321);
+            this.cbGender.Name = "cbGender";
+            this.cbGender.Size = new System.Drawing.Size(121, 24);
+            this.cbGender.TabIndex = 13;
+            // 
+            // cbRole
+            // 
+            this.cbRole.FormattingEnabled = true;
+            this.cbRole.Items.AddRange(new object[] {
+            "Editor",
+            "Intern",
+            "Developer",
+            "Department Manager",
+            "Engineer",
+            "Security"});
+            this.cbRole.Location = new System.Drawing.Point(160, 377);
+            this.cbRole.Name = "cbRole";
+            this.cbRole.Size = new System.Drawing.Size(121, 24);
+            this.cbRole.TabIndex = 14;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(415, 58);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(559, 243);
+            this.listView1.TabIndex = 15;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(516, 363);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 30);
+            this.btnAdd.TabIndex = 16;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(670, 363);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 30);
+            this.btnUpdate.TabIndex = 17;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(817, 363);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 30);
+            this.btnDelete.TabIndex = 18;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 90;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Surname";
+            this.columnHeader2.Width = 112;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Gender";
+            this.columnHeader5.Width = 102;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Role";
+            this.columnHeader6.Width = 70;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 479);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.cbRole);
+            this.Controls.Add(this.cbGender);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtboxAddress);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtboxPhone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtboxSurname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtboxName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtboxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtboxSurname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtboxPhone;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtboxAddress;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbGender;
+        private System.Windows.Forms.ComboBox cbRole;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+    }
+}
+
---
+++ EmployeeSystem/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace EmployeeSystem
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection = new SqlConnection("Data Source=DESKTOP-G2ISOTR;Initial Catalog=employeeSystem;Integrated Security=True");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowEmployees()
+        {
+            if(connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = "select * from employeesDB";
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    ListViewItem item = new ListViewItem();
+                    item.Text = reader["employeeName"].ToString();
+                    item.SubItems.Add(reader["employeeSurname"].ToString());
+                    item.SubItems.Add(reader["employeeGender"].ToString());
+                    item.SubItems.Add(reader["employeeRole"].ToString());
+
+                    listView1.Items.Add(item);
+                }
+                connection.Close();
+            }
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = "insert into employeesDB (employeeName, employeeSurname, employeePhone, employeeAddress, employeeGender, employeeRole) values ('" + txtboxName.Text + "', '" + txtboxSurname.Text + "', '" + txtboxPhone.Text + "', '" + txtboxAddress.Text + "', '" + cbGender.Text + "', '" + cbRole.Text + "')";
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+
+                connection.Close();
+
+                MessageBox.Show("New Employee has successfully registered.");
+                listView1.Items.Clear();
+                ShowEmployees();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowEmployees();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to delete", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "delete from employeesDB where employeeName = @employeeName";
+                    cmd.Parameters.AddWithValue("@employeeName", listView1.SelectedItems[0].Text);
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+
+                    connection.Close();
+                    listView1.Items.Clear();
+                    ShowEmployees();
+                    MessageBox.Show("Employees was successfully deleted.");
+                }
+            }
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            txtboxName.Text = listView1.SelectedItems[0].SubItems[0].Text;
+            txtboxSurname.Text = listView1.SelectedItems[0].SubItems[1].Text;
+            cbGender.Text = listView1.SelectedItems[0].SubItems[2].Text;
+            cbRole.Text = listView1.SelectedItems[0].SubItems[3].Text;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if(connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = "update employeesDB set employeeName='"+txtboxName.Text+"', employeeSurname='"+txtboxSurname.Text+"', employeeGender='"+cbGender.Text+"', employeeRole='"+cbRole.Text+"' where employeeName=@id";
+                cmd.Parameters.AddWithValue("@id", listView1.SelectedItems[0].Text);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+
+                connection.Close();
+                listView1.Items.Clear();
+                ShowEmployees();
+            }
+        }
+    }
+}
